a55e9ae4a6cfc1393ea2d3b995aea3c19e12fe4c
Jasmine <104405058+jaasc@users.noreply.github.com>
2023-01-20 10:16:12 +0800

    Updated style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,9 +16,7 @@ module.exports = {
         'white': 'hsl(0, 0%, 100%)',
         'marineBlue': 'hsl(213, 96%, 18%)',
         'purplishBlue': 'hsl(243, 100%, 62%)',
-        'pastelBlue': 'hsl(228, 100%, 84%)',
         'lightBlue': 'hsl(206, 94%, 87%)',
-        'strawberryRed': 'hsl(354, 84%, 57%)',
       },
       backgroundImage: {
         'sidebar-desktop': "url('./assets/images/bg-sidebar-desktop.svg')",
